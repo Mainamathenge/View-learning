2ae3e560b865d2139d55f71d5115b03118063f32
Mainamathenge <mainamathengej@gmail.com>
2023-07-02 22:05:33 +0300

    section 8

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <header><h1>My friends</h1></header>
+  <ul>
+    <friend-contact></friend-contact>
+    <friend-contact></friend-contact>
+  </ul>
+</template>
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+          friends: [
+      {
+        id: "Manuel",
+        name: "Manuel Lorenz",
+        phone: "097369379",
+        email: "manuel@localost.com",
+      },
+      {
+        id: "Joelin",
+        name: "joelin Wangai",
+        phone: "097369379",
+        email: "joelin@localost.com",
+      },
+    ],
+    }
+
+  },
+};
+</script>
