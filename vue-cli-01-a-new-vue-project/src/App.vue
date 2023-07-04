9be768fb6d84cf919f8e43b36b778d4af0c67d8b
Mainamathenge <mainamathengej@gmail.com>
2023-07-04 14:25:40 +0300

    inputs

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -1,32 +1,140 @@
 <template>
   <header><h1>My friends</h1></header>
+  <new-friend></new-friend>
   <ul>
-    <friend-contact></friend-contact>
-    <friend-contact></friend-contact>
+    <friend-contact
+      v-for="friend in friends"
+      :key="friend.id"
+      :id="friend.id"
+      :name="friend.name"
+      :phone-number="friend.phone"
+      :email-address="friend.email"
+      :is-favorite="friend.isFavorite"
+      @toggle-favorite="changeFavorite"
+    ></friend-contact>
   </ul>
 </template>
 <script>
-import FriendContact from './components/FriendContact.vue';
+import FriendContact from "./components/FriendContact.vue";
+import NewFriend from "./components/NewFriend.vue";
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
-          friends: [
-      {
-        id: "Manuel",
-        name: "Manuel Lorenz",
-        phone: "097369379",
-        email: "manuel@localost.com",
-      },
-      {
-        id: "Joelin",
-        name: "joelin Wangai",
-        phone: "097369379",
-        email: "joelin@localost.com",
-      },
-    ],
+      friends: [
+        {
+          id: "Manuel",
+          name: "Manuel Lonz",
+          phone: "097369379",
+          email: "manuel@localost.com",
+          isFavorite: true,
+        },
+        {
+          id: "Joelin",
+          name: "joelin Wangai",
+          phone: "097369379",
+          email: "joelin@localost.com",
+          isFavorite: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    changeFavorite(friendId) {
+      const changeStatus = this.friends.find(
+        (friend) => friendId === friend.id
+      );
+      console.log(changeStatus);
+      changeStatus.isFavorite = !changeStatus.isFavorite;
+    },
+    addContact(name, phone,email) {
+      const NewFriendContact ={
+        id: new Date().toISOString(),
+        name : name,
+        phone : phone ,
+        email : email
+      }
     }
-
   },
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li,
+#app form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+</style>
