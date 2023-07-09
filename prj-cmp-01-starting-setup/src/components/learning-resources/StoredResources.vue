26867bba60ffaa06c2997fad21e9f477b2dcdbe9
Mainamathenge <mainamathengej@gmail.com>
2023-07-09 22:59:39 +0300

    project1

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <learning-resources
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.id"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+export default {
+  props:['resources']
+}
+</script>
