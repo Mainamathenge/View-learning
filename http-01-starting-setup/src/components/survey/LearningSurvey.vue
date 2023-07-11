d6cb7848f4743c3f880e00f3a12590e309b57ecf
Mainamathenge <mainamathengej@gmail.com>
2023-07-11 11:56:17 +0300

    moving

--- http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -2,14 +2,20 @@
   <section>
     <base-card>
       <h2>How was you learning experience?</h2>
-      <form @submit.prevent="submitSurvey">
+      <form @submit="submitSurvey">
         <div class="form-control">
           <label for="name">Your Name</label>
           <input type="text" id="name" name="name" v-model.trim="enteredName" />
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
@@ -23,12 +29,18 @@
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -46,7 +58,7 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
+  //emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -55,11 +67,23 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
-
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
+      fetch(
+        'https://vue-js-backend-634cd-default-rtdb.firebaseio.com/surveys.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: this.enteredName,
+            rating: this.chosenRating,
+          }),
+        }
+      );
       this.enteredName = '';
       this.chosenRating = null;
     },
